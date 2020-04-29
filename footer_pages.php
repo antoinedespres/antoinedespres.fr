4af90d0323cbc2cd62c00ecd1395b794ad75e24e
AntoineD75 <58844676+AntoineD75@users.noreply.github.com>
2020-04-29 20:15:22 +0200

    Improved design

---
+++ footer_pages.php
@@ -0,0 +1,5 @@
+        <footer>
+            <h1>Antoine Després - 2020</h1>
+            <hr />
+            <p><a href="../contact/">Contact</a><br><a href="https://www.linkedin.com/in/antoine-despr%C3%A9s-89614718b/"><img src="../images/linkedin_icon.png" alt="Connectons-nous sur LinkedIn !" /></a><br /> <a href="../legal/">Mentions légales</a><br></p>
+        </footer>
